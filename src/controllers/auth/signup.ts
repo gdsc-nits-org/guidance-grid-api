70487fdc33bc451aac73ffd051e7eb1d761b5c53
Jaydeep Das <jaydeepjd.8914@gmail.com>
2023-02-28 18:04:17 +0530

    FIX: Async Error handlers

--- src/controllers/auth/signup.ts
+++ src/controllers/auth/signup.ts
@@ -1,19 +1,23 @@
 import * as Interfaces from "@interfaces";
 import * as Utils from "@utils";
 
-const signup: Interfaces.Middlewares.Async = async (req, res) => {
+const signup: Interfaces.Middlewares.Async = async (req, res, next) => {
   const body: Interfaces.User = req.body;
-  const user = await Utils.Auth.createUser(body);
-  if (req.session) {
-    req.session.user = { uid: user.id, username: user.uname };
+  try {
+    const user = await Utils.Auth.createUser(body);
+    if (req.session) {
+      req.session.user = { uid: user.id, username: user.uname };
+    }
+    return res.json(
+      Utils.Response.success({
+        uid: user.id,
+        username: user.uname,
+        name: user.name,
+      })
+    );
+  } catch (error) {
+    return next(error);
   }
-  return res.json(
-    Utils.Response.success({
-      uid: user.id,
-      username: user.uname,
-      name: user.name,
-    })
-  );
 };
 
 export default signup;
