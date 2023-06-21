5220a4052c23d738233639102da279ab2333a2b2
Jaydeep Das <jaydeepjd.8914@gmail.com>
2023-06-22 00:02:41 +0530

    ADD: IP Rate Limiting with Redis

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,5 +2,13 @@ import * as Error from "./error";
 import isLoggedIn from "./auth";
 import { validate } from "./validation";
 import { SIGNUP_VALIDATORS, LOGIN_VALIDATORS } from "./validators";
+import limiter from "./ratelimit";
 
-export { Error, validate, isLoggedIn, SIGNUP_VALIDATORS, LOGIN_VALIDATORS };
+export {
+  Error,
+  validate,
+  isLoggedIn,
+  SIGNUP_VALIDATORS,
+  LOGIN_VALIDATORS,
+  limiter,
+};
