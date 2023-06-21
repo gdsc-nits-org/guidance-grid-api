08f67e9dc9b87aa7cf6258d2007c6b05c5d74059
Jaydeep Das <jaydeepjd.8914@gmail.com>
2023-06-21 23:10:12 +0530

    UPD: Add esbuild file for better interop

---
+++ esbuild.mjs
@@ -0,0 +1,15 @@
+import esbuild from "esbuild"
+
+const buildDir = "build"
+const buildEntry = "handler.js"
+const distDir = "dist"
+const distOutfile = "main.js"
+
+esbuild.build({
+    entryPoints: [`./${buildDir}/${buildEntry}`],
+    bundle: true,
+    platform: "node",
+    target: "node18",
+    outfile: `./${distDir}/${distOutfile}`,
+    minify: true,
+}).catch(() => process.exit(1))
